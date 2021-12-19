835b31d93d7d3c18974bea692c7f01755f8c20e4
b00601118 <bahadir.colak@huawei.com>
2021-12-19 16:58:35 +0300

    receive, callback and publish class has been added

--- pubsub/pubsub.py
+++ pubsub/pubsub.py
@@ -1,4 +1,6 @@
 import json
+import time
+
 import pika
 
 
@@ -15,10 +17,21 @@ class PubSub:
         return pika.BlockingConnection(connection_param)
 
     def publish(self, solution):
-        pass
+        self.channel.exchange_declare(exchange='solutions', exchange_type='topic')
+        self.channel.basic_publish(
+            exchange='solutions', routing_key='travel.salesman', body=solution)
 
     def receive(self):
-        pass
+        self.channel.exchange_declare(exchange='problems', exchange_type='topic')
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        queue_name = result.method.queue
+        self.channel.queue_bind(exchange='problems', queue=queue_name, routing_key='#')
+        self.channel.basic_consume(
+            queue=queue_name, on_message_callback=self.callback, auto_ack=True)
+        self.channel.start_consuming()
 
-    def callback(self):
-        pass
+    def callback(self, ch, method, properties, body):
+        # Let's give some time for connection (Only for developing purposes)
+        time.sleep(0.3)
+        data = json.loads(body.decode())
+        print(f'Received Problem: {data}')
