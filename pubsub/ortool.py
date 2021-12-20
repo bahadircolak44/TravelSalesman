013eed09f4be6827373cf096165fd37f54b172ee
b00601118 <bahadir.colak@huawei.com>
2021-12-20 11:27:32 +0300

    exception block added into ortool

--- pubsub/ortool.py
+++ pubsub/ortool.py
@@ -38,48 +38,51 @@ def return_solution(num_vehicles, manager, routing, solution):
 
 
 def main(locations, num_vehicles=1, depot=0, max_travel_distance=1000):
-    """Entry point of the program."""
-    # Create the routing index manager.
-    manager = pywrapcp.RoutingIndexManager(len(locations),
-                                           num_vehicles, depot)
-    # Create Routing Model.
-    routing = pywrapcp.RoutingModel(manager)
+    try:
+        """Entry point of the program."""
+        # Create the routing index manager.
+        manager = pywrapcp.RoutingIndexManager(len(locations),
+                                               num_vehicles, depot)
+        # Create Routing Model.
+        routing = pywrapcp.RoutingModel(manager)
 
-    distance_matrix = compute_euclidean_distance_matrix(locations)
+        distance_matrix = compute_euclidean_distance_matrix(locations)
 
-    # Create and register a transit callback.
-    def distance_callback(from_index, to_index):
-        """Returns the distance between the two nodes."""
-        # Convert from routing variable Index to distance matrix NodeIndex.
-        from_node = manager.IndexToNode(from_index)
-        to_node = manager.IndexToNode(to_index)
-        return distance_matrix[from_node][to_node]
+        # Create and register a transit callback.
+        def distance_callback(from_index, to_index):
+            """Returns the distance between the two nodes."""
+            # Convert from routing variable Index to distance matrix NodeIndex.
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            return distance_matrix[from_node][to_node]
 
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+        transit_callback_index = routing.RegisterTransitCallback(distance_callback)
 
-    # Define cost of each arc.
-    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+        # Define cost of each arc.
+        routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-    # Add Distance constraint.
-    dimension_name = 'Distance'
-    routing.AddDimension(
-        transit_callback_index,
-        0,  # no slack
-        max_travel_distance,  # vehicle maximum travel distance
-        True,  # start cumul to zero
-        dimension_name)
-    distance_dimension = routing.GetDimensionOrDie(dimension_name)
-    distance_dimension.SetGlobalSpanCostCoefficient(100)
+        # Add Distance constraint.
+        dimension_name = 'Distance'
+        routing.AddDimension(
+            transit_callback_index,
+            0,  # no slack
+            max_travel_distance,  # vehicle maximum travel distance
+            True,  # start cumul to zero
+            dimension_name)
+        distance_dimension = routing.GetDimensionOrDie(dimension_name)
+        distance_dimension.SetGlobalSpanCostCoefficient(100)
 
-    # Setting first solution heuristic.
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+        # Setting first solution heuristic.
+        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
 
-    # Solve the problem.
-    solution = routing.SolveWithParameters(search_parameters)
+        # Solve the problem.
+        solution = routing.SolveWithParameters(search_parameters)
 
-    # Print solution on console.
-    if solution:
-        return return_solution(num_vehicles, manager, routing, solution)
-    return {}
+        # Print solution on console.
+        if solution:
+            return return_solution(num_vehicles, manager, routing, solution)
+        return {}
+    except Exception as ex:
+        return {'error': str(ex)}
